6a116fe2a603e3e5baf28fd0a55313d6019a1fe5
Joanna Wang <joannawang1027@gmail.com>
2024-11-10 17:19:38 -0800

    Initial commit

---
+++ src/test/java/com/example/receiptprocessorchallengejoannawang/ReceiptProcessorChallengeJoannaWangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.receiptprocessorchallengejoannawang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptProcessorChallengeJoannaWangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
